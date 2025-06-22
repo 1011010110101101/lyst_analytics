047cff3afdfca62f028e58ed95d43feee9a5a743
Patrick Gallagher <patgalicloud.net@icloud.com>
2025-06-22 16:26:24 +0000

    deleted old tests

--- tests/invalid_rule_configurations.sql
+++
@@ -1,6 +0,0 @@
--- Test that no rows exist where BOTH allowed and disallowed countries are set
-
-select *
-from {{ ref('stg_products') }}
-where array_size(allowed_countries) > 0
-  and array_size(disallowed_countries) > 0
