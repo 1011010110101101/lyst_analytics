197ee5926f3e23a6411d992aa860afc10826a6e3
Patrick Gallagher <patgalicloud.net@icloud.com>
2025-06-22 13:20:40 +0000

    .

--- models/staging/stg_tracks_raw.sql
+++ models/staging/stg_tracks_raw.sql
@@ -1,25 +1,10 @@
-{{ config(
-    materialized='view'
-) }}
+{{ config(materialized='view') }}
 
-with source as (
-    select * from {{ source('data_source', 'tracks_raw') }}
-),
-deduplicated as (
-    select *
-    from (
-        select *,
-               row_number() over (partition by track_id order by insert_timestamp desc) as row_num
-        from source
-    )
-    where row_num = 1
-)
-
-select
-    track_id,
+SELECT
+    insert_timestamp,
     track_timestamp,
+    track_id,
     product_id,
     retailer_id,
-    price,
-    insert_timestamp
-from deduplicated
+    price
+FROM {{ source('tracks', 'tracks_raw') }}
