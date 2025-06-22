48d7417d37ad56abf4197520c39c090c68a21b21
Patrick Gallagher <patgalicloud.net@icloud.com>
2025-06-22 15:40:38 +0000

    .

--- models/marts/product_countries.sql
+++ models/marts/product_countries.sql
@@ -1,35 +1,42 @@
--- models/marts/product_countries.sql
 {{ config(materialized='table') }}
 
 with countries as (
-    select * from {{ ref('stg_countries') }}
+    select
+        country_code
+    from {{ ref('stg_countries') }}
 ),
 
 products as (
-    select * from {{ ref('stg_products') }}
+    select
+        product_id,
+        date,
+        allowed_countries,
+        disallowed_countries
+    from {{ ref('stg_products') }}
 ),
 
 exploded as (
     select
-        prod.product_id,
-        prod.date,
-        ctry.country_code,
+        products.product_id,
+        products.date,
+        countries.country_code,
         case
-            when array_size(prod.allowed_countries) > 0 then
+            when array_size(products.allowed_countries) > 0 then
                 case
-                    when ctry.country_code = ANY(prod.allowed_countries) then 'allowed'
+                    when countries.country_code = ANY(products.allowed_countries) then 'allowed'
                     else 'unavailable'
                 end
-            when array_size(prod.disallowed_countries) > 0 then
+            when array_size(products.disallowed_countries) > 0 then
                 case
-                    when ctry.country_code = ANY(prod.disallowed_countries) then 'unavailable'
+                    when countries.country_code = ANY(products.disallowed_countries) then 'unavailable'
                     else 'allowed'
                 end
-            when array_size(prod.allowed_countries) = 0 and array_size(prod.disallowed_countries) = 0 then 'allowed'
+            when array_size(products.allowed_countries) = 0 and array_size(products.disallowed_countries) = 0 then 'allowed'
             else 'unavailable'
         end as availability
-    from products prod
-    cross join countries ctry
+    from products
+    cross join countries
 )
 
-select * from exploded
+select *
+from exploded
